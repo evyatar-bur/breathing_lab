b80861e5742299446ccb2e894d94c57978c2e3df
evyatar-bur <65293193+evyatar-bur@users.noreply.github.com>
2021-01-10 20:48:42 +0200

    main_commit

---
+++ Part_1.m
@@ -0,0 +1,83 @@
+%% Breathing lab - Part one
+close all
+clear 
+clc
+
+% Load data
+data_set = load('Part1_recording.mat');
+air_flow = data_set.data(:,1);
+air_volume = data_set.data(:,2);
+
+age = 25; % years
+height = 171; % cm
+
+
+% Calculating RV and VC using known approximation
+Expected_RV = (19.7*height + 20.1*age - 2421)/1000; % Liter 
+Expected_VC = (0.052*height - 0.022*age - 3.6); % Liter 
+
+%% Calculate volumes and capacities from the spirometry test
+
+% RV
+RV = min(air_volume); % Liter
+
+% TLC
+TLC = max(air_volume); % Liter
+
+% VC
+VC = TLC - RV; % Liter
+
+% TV
+min_points_ind = find(islocalmin(air_volume,'MinSeparation',300));
+max_points_ind = find(islocalmax(air_volume,'MinSeparation',300));
+
+min_points = air_volume(min_points_ind);
+max_points = air_volume(max_points_ind);
+
+tidal_volumes = max_points(1:5) - min_points(1:5);
+
+TV = mean(tidal_volumes);
+
+TV_STD = std(tidal_volumes);
+
+% The last normal breathes were not taken because they are affected by the  
+
+% IRV
+inspiratory_residual_volumes = TLC - max_points(1:5);
+
+IRV = mean(inspiratory_residual_volumes);
+
+IRV_STD = std(inspiratory_residual_volumes);
+
+
+% ERV
+expiratory_residual_volumes = min_points(1:5) - RV;
+
+ERV = mean(expiratory_residual_volumes);
+
+ERV_STD = std(expiratory_residual_volumes);
+
+% IC
+IC = TV + IRV;
+
+% EC 
+EC = TV + ERV;
+
+% FRC
+FRC = RV + ERV;
+
+
+%% Show a spectrogram for the signal
+
+fs = 1/((data_set.isi)*(10^-3));
+
+figure(1)
+spectrogram(air_volume,120,80,10000,fs);
+title('Spectrogram of air volume measured in the spiromtry test')
+xlim([0 5])
+
+t = (0:length(air_volume)-1)/fs;
+
+
+
+
---
+++ Part_2.m
@@ -0,0 +1,87 @@
+%% Part 2 
+
+% Load data
+data_set_2 = load('Part2_recording.mat');
+
+resperation = data_set_2.data(:,2);
+
+fs = 1/((data_set_2.isi)*(10^-3));
+t = (0:length(resperation)-1)/fs;
+
+figure;
+plot(t,resperation)
+
+%% Section 1 - Resting state
+
+% Insperation duration (calculated manually)
+
+insp_durations = [2.69 2.4 2.2];
+
+insp_duration = mean(insp_durations);
+insp_STD = std(insp_durations);
+
+
+% Experation duration (calculated manually)
+
+exp_durations = [1.95 2.24 2.36];
+
+experation = mean(exp_durations);
+exp_STD = std(exp_durations);
+
+
+% Total breathing cycles durations (calculated manually)
+
+
+breath_durations = [4.91 4.89 4.87];
+
+total_breath_duration = mean(breath_durations);
+total_STD = std(breath_durations);
+
+% Calculate resting respration_rate
+
+respration_rates = (1./breath_durations).*60; % breath/minute
+
+RR_rest = mean(respration_rates);
+RR_rest_STD = std(respration_rates);
+
+
+%% Section 2 - Breathing cycle durations and RRs
+
+% Hyperventilation
+
+peaks_hyper = findpeaks(resperation(1700:4220));
+
+Hyper_breath_duration = ((4220-1700)/fs)/length(peaks_hyper);
+
+Hyper_RR = 60/Hyper_breath_duration;
+
+% Hypoventilation
+
+peaks_hypo = findpeaks(resperation(7692:10600),'MinPeakProminence',0.5);
+
+Hypo_breath_duration = ((10600-7692)/fs)/(length(peaks_hypo)+1);
+
+Hypo_RR = 60/Hypo_breath_duration;
+
+%% Cough and read
+
+peaks_C_R = findpeaks(resperation(13760:16710),'MinPeakProminence',0.5);
+
+C_R_breath_duration = ((16710-13760)/fs)/(length(peaks_C_R)+1);
+
+C_R_RR = 60/C_R_breath_duration;
+
+
+
+%% Section 3 - Computing coralation between signals
+
+figure;
+wcoherence(resperation(7700:9880),resperation(14540:16720));
+title('Cross spectrum of the hypoventilation section vs. the cough and read section')
+
+figure;
+wcoherence(resperation(1813:3993),resperation(14540:16720));
+title('Cross spectrum of the hyperventilation section vs. the cough and read section')
+
+
+
